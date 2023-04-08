2b692b88165b0896018f32166ec9501018d80e38
mariaelisa492 <mariaelisa004@hotmail.com>
2023-04-08 10:47:42 -0500

    Add git ignore

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+
+function App() {
+  return (
+      <div className="App">
+        <p>Prueba de renderizado</p>
+          <Navbar/>
+          <Footer/>
+      </div>
+  );
+}
+
+export default App;
